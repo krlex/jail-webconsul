b4251bccd487dfab875fc9dd9c2c6ec63bc68bd9
Goran Mekić <meka@tilda.center>
2017-12-17 16:11:40 +0100

    Upgrade to newer Reggae

--- playbook/roles/webconsul/files/notify-consul.sh
+++ playbook/roles/webconsul/files/notify-consul.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 DATE=`date +%s`
-/usr/local/bin/curl -X PUT --data-bin $DATE 'http://127.0.2.1:8500/v1/kv/letsencrypt/timestamp'
+/usr/local/bin/curl -X PUT --data-bin $DATE 'http://consul.my.domain:8500/v1/kv/letsencrypt/timestamp'
